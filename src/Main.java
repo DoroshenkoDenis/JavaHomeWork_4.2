0dc427424a099b635a7dbbf1786901e4652656ca
Denis Doroshenko <ddtlt1984@gmail.com>
2021-03-13 10:47:04 +0400

    Code Review

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        double mass = 90.5;
+        double height = 1.9;
         BmiService service = new BmiService();
-        double index = service.calculate(90, 1.9);
-        System.out.println(index);
+        double index = service.calculate(mass, height);
         System.out.printf("%.2f", index);
     }
 }
