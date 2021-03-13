921d41243c024331957b8f4e87fffa5aa6c8d657
DoroshenkoDenis <47859608+DoroshenkoDenis@users.noreply.github.com>
2021-03-13 09:45:02 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         double index = service.calculate(90, 1.9);
-        System.out.println(index);
         System.out.printf("%.2f", index);
     }
 }
